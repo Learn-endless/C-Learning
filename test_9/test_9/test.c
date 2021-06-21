f87d51822dddad65e0cacf1c658060fc9b1ff550
xwj <1983318249@qq.com>
2021-06-21 16:11:53 +0800

    6.21

--- test_9/test_9/test.c
+++ test_9/test_9/test.c
@@ -1,13 +1,30 @@
 #define _CRT_SECURE_NO_WARNINGS 1
-#include<stdio.h>
-int main()
-{
-	char a = -1;
-	signed char b = -1;
-	unsigned char c = -1;
-	printf("%d %d %d\n", a, b, c);
-	return 0;
-}
+
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	char a = -128;     //一个char范围：-128~127
+//	printf("%u\n", a);
+//	char b = 128;      //128=127+1(char最大放127)
+//	printf("%u\n", b); //1111 1111 1111 1111 1111 1111 1000 0000
+//	printf("%d\n", a);
+//	printf("%d\n", b);
+//	return 0;
+//}
+
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	char a = -1;
+//	signed char b = -1;
+//	unsigned char c = -1;
+//	printf("%d %d %d\n", a, b, c);
+//	return 0;
+//}
 
 
 
